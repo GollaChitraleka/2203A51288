ca09715c319c028a46b278b82853e592c8e00873
GollaChitraleka <144619261+GollaChitraleka@users.noreply.github.com>
2025-06-12 12:35:16 +0530

    Delete question2 directory

--- question2/server.js
+++
@@ -1,9 +0,0 @@
-// Required Libraries
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const PORT = 9875;
-const cache = new Map();
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
